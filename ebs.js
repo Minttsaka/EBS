22ee39c6efb9e98d06b608ba6cb0915ccd36e282
Mint <minttsaka@gmail.com>
2023-06-16 13:07:16 +0200

    new update

--- ebs.js
+++
@@ -1,6 +0,0 @@
-setTimeout(() => {
-    const wordAnimations = document.querySelectorAll('.word-animation');
-    wordAnimations.forEach((element, index) => {
-      element.style.animationDelay = `${index * 2}s`;
-    });
-  }, 20);
